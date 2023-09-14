a02353a4af9b25e577d6259dc60184462a6123ec
s66829020@gmail.com <s66829020@gmail.com>
2023-09-14 14:46:53 +0530

    first commit

---
+++ Data/BookDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Assignment_4.Models;
+
+namespace Assignment_4.Data
+{
+    public class BookDbContext : DbContext
+    {
+        public BookDbContext (DbContextOptions<BookDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Assignment_4.Models.Book> Book { get; set; } = default!;
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Assignment_4.Models
+{
+    [Table("Book")]
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }   
+    }
+}
